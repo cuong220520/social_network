8b41aa8cb4fc001be8af9bb2aab463e3b675cee4
cuong220520 <cuongNDGBS18195@fpt.edu.vn>
2020-03-30 15:12:06 +0700

    finish some basic function with firebase db

---
+++ dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createAt: '2020-03-30T08:04:00.601Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
